7f49085a6cbfa45f259d0264785c9b9c840e0b93
Yani Pontoni <yani.pontoni@gmail.com>
2021-10-07 17:14:55 -0300

    Commit de Models
    Subida la carpeta con los models del TP

---
+++ Models/Student.php
@@ -0,0 +1,15 @@
+<?php
+    namespace Models;
+
+    use Models\Person as Person;
+
+    class Student extends Person
+    {
+        private $studentId;
+        private $careerId;
+        
+
+        
+    }
+?>
+
