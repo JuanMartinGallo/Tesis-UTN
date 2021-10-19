56ae43ae01b7f9009dc12775cc1e5a32d131583f
ElDiegote <n17_94@hotmail.com>
2021-10-19 19:45:16 -0300

    primera entrega 1.0
    todos los puntos de la narrativa resueltos

--- Views/company-list.php
+++ Views/company-list.php
@@ -1,34 +1,28 @@
 <?php
 require_once('nav.php');
-
-use DAO\CompanyDAO as CompanyDAO;
-
-$companyDAO = new CompanyDAO();
 ?>
-
 <main class="py-5">
      <section id="listado" class="mb-5">
           <div class="container">
-               <form action="<?php echo FRONT_ROOT ?>Company/Filter" method="post">
+          <form action="<?php echo FRONT_ROOT ?>Company/Filter" method="post">
+                    
                     <div class="col-lg-3">
                          <div class="form-group">
-                              <label for="">Buscar:</label>
+                         <h4 class="mb-4">Buscar compañias</h4>
                               <input  type="text" name="name" value="" class="form-control">
                          </div>
-                    </div>
+                    </div>       
                </form>
-
                <h2 class="mb-4">Lista de compañias</h2>
-
                <table class="table bg-light-alpha">
                     <thead>
                          <th>Nombre</th>
                     </thead>
                     <tbody>
-
                          <?php foreach ($companyList as $company) { ?>
                               <tr>
                                    <td><?php echo $company->getName() ?></td>
+                                   <?php if($_SESSION['userLogged']->getRole() == "Admin"){ ?>
                                    <td>
                                         <form action="<?php echo FRONT_ROOT ?>Company/ShowDataView" method="post">
                                              <button type="submit" name='idCompany' value=<?php echo $company->getIdCompany() ?> class="btn btn-dark ml-auto d-block">Ver Perfil</button>
@@ -46,9 +40,10 @@ $companyDAO = new CompanyDAO();
                                    </td>
                               </tr>
                          <?php } ?>
-
+                         <?php }?>
                     </tbody>
                </table>
           </div>
+
      </section>
 </main>
