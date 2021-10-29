cab456ce464c180fff8ff18ee19f5e18f319ccd8
Yani Pontoni <yani.pontoni@gmail.com>
2021-10-28 23:30:45 -0300

    Update Schema.sql

--- Schema/Schema.sql
+++ Schema/Schema.sql
@@ -4,7 +4,7 @@ USE TPFinalLab4;
 
 CREATE TABLE companies
 (
-	companyId INT NOT NULL AUTO_INCREMENT,
+    companyId INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     cuit VARCHAR(20) NOT NULL,
     location VARCHAR(50) NOT NULL,
@@ -12,6 +12,8 @@ CREATE TABLE companies
 
     CONSTRAINT pk_company_id PRIMARY KEY (companyId)
 );
+
+INSERT INTO companies (name, cuit, location, phoneNumber) VALUES ('Globant', '30-458778-9', 'Mar del Plata', '223-636-2356'), ('Infosys', '30-666128-9', 'Mar del Plata', '223-636-9999'), ('Toledo', '32-258778-9', 'Mar del Plata', '223-625-2756');
 
 CREATE TABLE students
 (
@@ -39,4 +41,15 @@ CREATE TABLE admins
     email VARCHAR(30) NOT NULL,
 
     CONSTRAINT pk_admin_id PRIMARY KEY (adminId)
+);
+
+INSERT INTO admins (firstName, lastName, dni, email) VALUES ('Martin', 'Gallo', '40-568-4785', 'martin833@gmail.com'), ('Yani', 'Pontoni', '87-548-4722', 'yani.pontoni@gmail.com'), ('Diego', 'Arzondo', '12-148-4757', 'eldiegote2021@gmail.com');
+
+CREATE TABLE careers
+(
+    careerId INT NOT NULL AUTO_INCREMENT,
+    description VARCHAR(20) NOT NULL,
+    active boolean,
+
+    CONSTRAINT pk_career_id PRIMARY KEY (careerId)
 );
